dfbf3720395ddd4d6b209c5e5df49e065aa02f9b
XJ He <xj_he@intuit.com>
2016-03-17 14:39:52 -0400

    enable locale info

--- component/demo.js
+++ component/demo.js
@@ -4,6 +4,9 @@ import DateTimeField from "react-bootstrap-datetimepicker";
 import DateTimePicker from "react-bootstrap-datetimepicker";
 import DateTimePickerDate from "react-bootstrap-datetimepicker";
 import QBCalendarTimePicker from "./qbCalendarTimePicker";
+import DateTime from "react-datetime";
+import moment from "moment";
+import "moment/locale/ja";
 
 class Demo extends Component {
 	
@@ -12,19 +15,25 @@ class Demo extends Component {
 			<div className="container">
 		      <div className="row">
 	            <div className="col-xs-6">
-	              <h5>Example [DateTimeField widget]</h5>
-	              <DateTimeField defaultText="Please select a date"/>
+	              <h5>Example [DateTimeField widget] [react-bootstrap-datetimepicker]</h5>
+	              <DateTimeField defaultText="Please select a date" locale="ja"/>
 	            </div>
 	          </div>
 	          <hr/>
 	          <div className="row">
 	            <div className="col-xs-6">
-	              <h5>Example [DateTimePickerDate] under time mode</h5>
+	              <h5>Example [DateTimePickerDate] [react-bootstrap-datetimepicker] using time mode</h5>
 	              <DateTimePickerDate mode="time"/>
 	            </div>
 	          </div>
 	          <hr/>
-	          
+	          <div className="row">
+	            <div className="col-xs-6">
+	              <h5>Example [DateTime] [react-datetime] with Japanese local</h5>
+	              <DateTime locale="ja"/>
+	            </div>
+	          </div>
+	          <hr/>
 	          <div className="row">
 	          	<div className="col-xs-12">
 	              <h5>Example QBCalendarTimePicker with [react-bootstrap-datetimepicker] implementation</h5>
